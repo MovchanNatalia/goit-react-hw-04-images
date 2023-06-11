a9a8a7a0434891665d9f5ee1440739f1d51cdf0a
MovchanNatalia <Movchan.n2106@Gmail.com>
2023-06-11 20:24:44 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,39 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Modal.module.css';
+import { useEffect } from 'react';
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+function Modal({ largeImageURL, onToggleModal }) {
+  useEffect(() => {
+    const handleKeyDown = evt => {
+      if (evt.code === 'Escape') {
+        onToggleModal();
+      }
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+    window.addEventListener('keydown', handleKeyDown);
 
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onToggleModal();
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onToggleModal]);
+
+  const handleBackdropClick = evt => {
+    if (evt.currentTarget === evt.target) {
+      onToggleModal();
     }
   };
 
-  handleBackdropClick = e => {
-    if (e.currentTarget === e.target) {
-      this.props.onToggleModal();
-    }
-  };
-
-  render() {
-    const { largeImageURL } = this.props;
-
-    return (
-      <div className={styles.Overlay} onClick={this.handleBackdropClick}>
-        <div className={styles.Modal}>
-          <img src={largeImageURL} alt="" />
-        </div>
+  return (
+    <div className={styles.Overlay} onClick={handleBackdropClick}>
+      <div className={styles.Modal}>
+        <img src={largeImageURL} alt="" />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 Modal.propTypes = {
   onToggleModal: PropTypes.func.isRequired,
 };
+
+export default Modal;
